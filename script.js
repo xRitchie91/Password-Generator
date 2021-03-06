2dee8bfad881582a6d638c45b72e57ca7625d63f
xRitchie91 <74946954+xRitchie91@users.noreply.github.com>
2021-03-06 17:30:57 -0600

    Added character variables to js file

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Assignment code here
 
+// Deployed application generates password to user specification between 8-128 characters
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +17,15 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var characters = prompt("How many characters would you like in your password?");
+
+if (characters < 8) {
+  alert("Password length must be 8-128 characters");
+} 
+if (characters > 128) {
+alert("Password length must be 8-128 characters"); 
+} 
+if (characters > 8) {
+console.log('I want ${characters} characters in my password');
+}
